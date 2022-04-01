edaeb25d0ebdfa4fecbcbdb0b94d9d0d8ee155a9
Bianca Linca <bianca.maria210@gmail.com>
2022-04-01 04:30:46 +0300

    .

---
+++ src/classes/BookGenres.java
@@ -0,0 +1,6 @@
+package classes;
+
+public enum BookGenres {
+    FICTION, CLASSICS, MYSTERY, FANTASY, ROMANCE, SCI_FI
+
+}
---
+++ src/classes/Books.java
@@ -0,0 +1,65 @@
+package classes;
+
+public class Books {
+    int isbn;
+    String title;
+    String author;
+
+    private BookGenres bookGenres;
+    public Books() {
+    }
+
+    public Books(BookGenres bookGenres, int isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.bookGenres = bookGenres;
+
+    }
+
+    public BookGenres getBookGenres(){
+        return bookGenres;
+    }
+
+    public void setBookGenres(BookGenres bookGenres){
+        this.bookGenres = bookGenres;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString(){
+    return "Book:" + "isbn=" + isbn + ", title='" + title + '\'' + ", author='" + author + '\'' ;
+    }
+
+
+
+
+
+
+
+
+
+}
